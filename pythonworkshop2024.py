33dd36a53042a1b3356c401fc53d8b1f52950595
hannah438 <hyeunah.kang@gmail.com>
2024-08-16 16:00:37 -0500

    Create pythonworkshop2024.py
    added descriptions

---
+++ pythonworkshop2024.py
@@ -0,0 +1,24 @@
+import os
+import csv
+import requests
+import pandas as pd
+import numpy as np
+
+import plotly
+import plotly.express as px
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib_venn
+
+print('all packages have been imported successfully!')
+
+
+groupone = set(["oak","pecan","walnut","bald cypress","sycamore"])
+grouptwo = set(["pecan","walnut","black locust","elm"])
+
+matplotlib_venn.venn2((groupone, grouptwo), ("Park 1 Trees", "Park 2 Trees"))
+
+
+
+plt.show() # to see the plot
+plt.savefig("scatterplot.png") # to save the plot
